8392a36d6932c3f1afaabf2fcaaecc773fc32e46
Umit Turk <uyt.turk@gmail.com>
2022-02-24 01:38:03 +0300

    widget structure

--- lib/Week1/welcome/welcome_view.dart
+++ lib/Week1/welcome/welcome_view.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
 
 class WelcomeView extends StatelessWidget {
+  final appBarTitle = 'Instagram';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.favorite))],
+        title: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(Icons.wallpaper),
+            Text(
+              appBarTitle,
+              style: Theme.of(context)
+                  .primaryTextTheme
+                  .headline4!
+                  .copyWith(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
